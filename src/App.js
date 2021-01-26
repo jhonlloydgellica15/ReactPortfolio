cf66a509ff34dbc0ed284e3756ed3a9794b35077
Jhon Lloyd <jhonlloydgellica15@gmail.com>
2021-01-26 11:12:28 +0800

    added about me component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import "./App.css";
 import MyCarousel from "./components/Carousel/MyCarousel";
 import MyNavbar from "./components/NavBar/Navbar";
 import TitleMessage from "./components/TitleMessage/TitleMessage";
+import Fade from "react-reveal/Fade";
+import BgImg from "./assets/img/parallax/background.webp";
+import Container from "react-bootstrap/Container";
+import { Parallax } from "react-parallax";
+import About from "./pages/About/About";
 
 function App() {
   return (
@@ -9,6 +14,18 @@ function App() {
       <MyCarousel />
       <TitleMessage />
       <MyNavbar />
+
+      <div>
+        <Parallax blur={{ min: -30, max: 30 }} bgImage={BgImg} bgImageAlt="" strength={-200}>
+          <div>
+            <Container className="container-box rounded">
+              <Fade duration={500}>
+                <About />
+              </Fade>
+            </Container>
+          </div>
+        </Parallax>
+      </div>
     </div>
   );
 }
---
+++ src/pages/About/About.jsx
@@ -0,0 +1,66 @@
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Image from "react-bootstrap/Image";
+import Button from "react-bootstrap/Button";
+import Profile from "../../assets/img/profile/profile.webp";
+import "./About.css";
+
+const About = () => {
+  return (
+    <div id="about">
+      <div className="about">
+        <h1 className="pt-3 text-center font-details pb-3">ABOUT ME</h1>
+        <Container>
+          <Row className="pt-3 pb-5 align-items-center">
+            <Col xs={12} md={6}>
+              <Row className="justify-content-center mb-2 mr-2 ">
+                <Image className="profile justify-content-end" alt="profile" src={Profile} thumbnail fluid />
+              </Row>
+            </Col>
+            <Col xs={12} md={6}>
+              <Row className=" align-items-start p-2 my-details rounded">
+                Hello there! I am <strong>&nbsp;Jhon Lloyd Gellica</strong>A passionate programmer and a Learner, born and brought up in the Philippines. I am a Frontend Web Developer with the
+                following tech stacks Html:5, Css3, Javascript(es6), Bootstrap, PhP, ReactJs, MySql. In 2020, I successfully completed my Bachelor Degree, 'Bachelor of Science in Information
+                Technology' at Holy Cross of Davao College. <br /> I'm a Dedicated, Hardworking, Detail Oriented and Passionate. Along with that, I also like building practice softwares or
+                applications like augmented reality. I love learning about new technologies, what bugs or errors are they encountered and how can I use them to build a better software.
+                <Col className="d-flex justify-content-center flex-wrap">
+                  <div>
+                    <a href="#contact">
+                      <Button className="m-2" variant="outline-primary">
+                        Let's talk
+                      </Button>
+                    </a>
+                  </div>
+                  <div>
+                    <a href="https://drive.google.com/file/d/1Snbq3x4Itk_ZN4c8J96aNdp4SunXR08Y/view?usp=sharing" target="_blank" rel="noopener noreferrer">
+                      <Button className="m-2" variant="outline-success">
+                        My Resume
+                      </Button>
+                    </a>
+                  </div>
+                  <div>
+                    <a href="https://github.com/akjha96" target="_blank" rel="noopener noreferrer">
+                      <Button className="m-2" variant="outline-dark">
+                        GitHub
+                      </Button>
+                    </a>
+                  </div>
+                  <div>
+                    <a href="https://www.linkedin.com/in/anand-kumar-jha-745798a4/" target="_blank" rel="noopener noreferrer">
+                      <Button className="m-2" variant="outline-info">
+                        LinkedIn
+                      </Button>
+                    </a>
+                  </div>
+                </Col>
+              </Row>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    </div>
+  );
+};
+
+export default About;
